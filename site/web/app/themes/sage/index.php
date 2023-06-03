a546a46e3583517198cf55cad8e665d1d9523e4c
aitor <aitor@e451.net>
2023-06-03 13:45:49 +0200

    Sage SVG y Sage directives

--- site/web/app/themes/sage/index.php
+++ site/web/app/themes/sage/index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,6 +8,7 @@
   </head>
 
   <body <?php body_class(); ?>>
+    <div id="fondo"></div>
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
@@ -17,4 +19,5 @@
     <?php do_action('get_footer'); ?>
     <?php wp_footer(); ?>
   </body>
+
 </html>
