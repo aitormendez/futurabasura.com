1bfd735e918fee7555319f2970fb3ffb1a681c54
Aitor Méndez <aitor@e451.net>
2024-08-08 19:42:32 +0200

    Footer: corrección CSS

--- site/web/app/themes/sage/resources/views/sections/footer.blade.php
+++ site/web/app/themes/sage/resources/views/sections/footer.blade.php
@@ -1,21 +1,21 @@
 <footer class="flex flex-wrap">
   
-    <div class="flex flex-wrap w-full md:w-1/2 bg-white font-bugrino md:text-xl">
-        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6 border-r border-b border-black">
+    <div class="flex flex-wrap w-full md:w-1/2 bg-white font-bugrino md:text-xl font-light">
+        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6 border-r-2 border-b-2 border-black">
             @foreach ($contents_nav as $item)
                 <li>
-                    <a href="{{ $item->url }}" class="hover:text-azul">{{ $item->label }}</a>
+                    <a href="{{ $item->url }}" class="hover:text-azul font-light">{{ $item->label }}</a>
                 </li>
             @endforeach
         </ul>
-        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6 border-b border-black">
+        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6 border-b-2 border-black">
             @foreach ($shop_nav as $item)
                 <li>
                     <a href="{{ $item->url }}" class="hover:text-azul">{{ $item->label }}</a>
                 </li>
             @endforeach
         </ul>
-        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6  border-r border-black">
+        <ul class="w-1/2 h-[50vw] md:h-[25vw] p-6 border-r-2 border-black">
             @foreach ($social_nav as $item)
                 <li>
                     <a href="{{ $item->url }}" class="hover:text-azul">{{ $item->label }}</a>
@@ -32,11 +32,11 @@
     </div>
 
     {{-- desktop --}}
-    <div class="hidden md:flex flex-col justify-between items-center w-full md:w-1/2 md:border-l border-black p-6 font-bugrino" style="background-color: {{ get_field('footer_color', 'option') }}">
+    <div class="hidden md:flex flex-col justify-between items-center w-1/2 border-l-2 border-black p-6 font-bugrino" style="background-color: {{ get_field('footer_color', 'option') }}">
         <div class="w-1/3 mt-[5vw]">
             @svg('images.logo-fb')
         </div>
-        <div class="text-xl text-center mb-[3vw]">
+        <div class="text-xl text-center mb-[3vw] font-light">
             {!! wpautop(get_field('footer_texto_mancha', 'option')) !!}
         </div>
     </div>
@@ -46,7 +46,7 @@
     <div class="flex md:!hidden flex-wrap w-full font-bugrino">
         <div class="arriba flex flex-wrap w-full h-[50vw] my-4">
             <div class="izq flex justify-end items-stretch w-1/2 bg-white">
-                <div class="w-[10vw] border-r border-black" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
+                <div class="w-[10vw] border-r border-black border-2" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
             </div>
             <div class="der flex justify-center items-center w-1/2" style="background-color: {{ get_field('footer_color', 'option') }}">
                     @svg('images.logo-fb', 'w-1/2')
@@ -54,7 +54,7 @@
         </div>
         <div class="abajo flex flex-wrap w-full h-[50vw]">
             <div class="izq flex justify-end items-stretch w-1/2 bg-white">
-                <div class="w-[10vw] border-r border-black" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
+                <div class="w-[10vw] border-r border-black border-2" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
             </div>
             <div class="der flex w-1/2 text-sm p-4" style="background-color: {{ get_field('footer_color', 'option') }}">
                 {!! wpautop(get_field('footer_texto_mancha', 'option')) !!}
@@ -70,7 +70,7 @@
         <ul class="p-6 font-bugrino flex gap-4 w-full text-xl justify-center mb-20">
             @foreach ($legal_nav as $item)
                 <li class="">
-                    <a href="{{ $item->url }}" class="hover:text-azul">{{ $item->label }}</a>
+                    <a href="{{ $item->url }}" class="hover:text-azul font-light">{{ $item->label }}</a>
                 </li>
             @endforeach
         </ul>
@@ -105,7 +105,7 @@
     <div class="flex md:!hidden flex-wrap w-full bg-white">
         <div class="arriba flex flex-wrap w-full h-[20px]">
             <div class="izq flex justify-end items-stretch w-1/2 bg-white">
-                <div class="w-[10vw] border-r border-black" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
+                <div class="w-[10vw] border-r border-black border-2" style="background-color: {{ get_field('footer_color', 'option') }}"></div>
             </div>
             <div class="der flex justify-center items-center w-1/2" style="background-color: {{ get_field('footer_color', 'option') }}">
             </div>
@@ -114,7 +114,7 @@
         <ul class="px-6 font-bugrino flex flex-col w-1/2">
             @foreach ($legal_nav as $item)
                 <li class="">
-                    <a href="{{ $item->url }}" class="hover:text-azul">{{ $item->label }}</a>
+                    <a href="{{ $item->url }}" class="hover:text-azul font-light">{{ $item->label }}</a>
                 </li>
             @endforeach
         </ul>
