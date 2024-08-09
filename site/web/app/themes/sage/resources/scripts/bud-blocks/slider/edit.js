11d946b2d4cb807382e8be3ca3fc40d47d7adb35
Aitor Méndez <aitor@e451.net>
2024-08-09 19:31:37 +0200

    Slider Block: Glide con marcado hardcoded funcionando OK

--- site/web/app/themes/sage/resources/scripts/bud-blocks/slider/edit.js
+++ site/web/app/themes/sage/resources/scripts/bud-blocks/slider/edit.js
@@ -1,11 +1,26 @@
 import { InnerBlocks, useBlockProps, RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+import { useEffect, useRef } from 'react';
+import Glide from '@glidejs/glide';
 
 const Edit = ({ attributes, setAttributes }) => {
   const blockProps = useBlockProps();
   const { title, textAlign, fontSize, lineHeight, fontFamily } = attributes;
-  console.log('blockProps', blockProps);
-  console.log(attributes);
+  const glideRef = useRef(null);
+
+  useEffect(() => {
+    if (glideRef.current) {
+      const glide = new Glide(glideRef.current, {
+        type: 'carousel',
+        perView: 3,
+        focusAt: 'center',
+      });
+
+      glide.mount();
+
+      return () => glide.destroy(); // Cleanup para evitar fugas de memoria
+    }
+  }, []);
 
   return (
     <div {...blockProps}>
@@ -15,7 +30,6 @@ const Edit = ({ attributes, setAttributes }) => {
           value={title}
           onChange={(newTitle) => {
             setAttributes({ title: newTitle });
-            console.log(blockProps);
           }}
           placeholder={__('Enter title...', 'textdomain')}
           style={{
@@ -25,7 +39,21 @@ const Edit = ({ attributes, setAttributes }) => {
             fontFamily: fontFamily,
           }}
         />
-        <div className="slider">{/* <InnerBlocks /> */}</div>
+
+        <div ref={glideRef} className="glide">
+          <div className="glide__track" data-glide-el="track">
+            <ul className="glide__slides">
+              <li className="glide__slide">0</li>
+              <li className="glide__slide">1</li>
+              <li className="glide__slide">2</li>
+            </ul>
+          </div>
+        </div>
+
+        <div data-glide-el="controls">
+          <button data-glide-dir="<<">Start</button>
+          <button data-glide-dir=">>">End</button>
+        </div>
       </div>
     </div>
   );
