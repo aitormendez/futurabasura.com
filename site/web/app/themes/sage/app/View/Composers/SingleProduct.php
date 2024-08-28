90139063a3ed7ca1ebea277574e445a18bd8bedc
Aitor Méndez <aitor@e451.net>
2024-08-28 15:09:24 +0200

    Single product: que no de error si no tiene artista asignado

--- site/web/app/themes/sage/app/View/Composers/SingleProduct.php
+++ site/web/app/themes/sage/app/View/Composers/SingleProduct.php
@@ -35,46 +35,48 @@ class SingleProduct extends Composer
             'is_on_sale'    => false,
         ];
 
-        if ( $product->is_on_sale() )  {
+        if ($product->is_on_sale()) {
             $output['is_on_sale'] = true;
             $output['sale_price'] = $product->get_sale_price();
         }
 
         return $output;
-
     }
 
     public function artista()
     {
         global $post;
         global $product;
+        $rand_posts = [];
         $artist = get_the_terms($post->ID, 'artist');
-        $rand_posts = get_posts([
-            'post_type'    => 'product',
-            'orderby'      => 'rand',
-            'post__not_in' => [$product->get_id()],
-            'tax_query'    => [
-                [
-                    'taxonomy' => 'artist',
-                    'field'    => 'term_id',
-                    'terms'     => $artist[0]->term_id
-                ]
-            ],
-        ]);
+        if ($artist) {
+            $rand_posts = get_posts([
+                'post_type'    => 'product',
+                'orderby'      => 'rand',
+                'post__not_in' => [$product->get_id()],
+                'tax_query'    => [
+                    [
+                        'taxonomy' => 'artist',
+                        'field'    => 'term_id',
+                        'terms'     => $artist[0]->term_id
+                    ]
+                ],
+            ]);
+        }
 
         if (! empty($rand_posts)) {
-            $rand_products_galleries = array_map( function( $post ) {
+            $rand_products_galleries = array_map(function ($post) {
                 $product = wc_get_product($post);
                 $attachment_ids = $product->get_gallery_image_ids();
 
 
-                $product_gallery = array_map( function( $att_id ) {
+                $product_gallery = array_map(function ($att_id) {
 
-                    $meta = get_post_meta( $att_id );
+                    $meta = get_post_meta($att_id);
 
                     $array =  [
-                        'att_url'    => wp_get_attachment_url( $att_id ),
-                        'att_srcset' => wp_get_attachment_image_srcset( $att_id ),
+                        'att_url'    => wp_get_attachment_url($att_id),
+                        'att_srcset' => wp_get_attachment_image_srcset($att_id),
                         'has_alt'    => false,
                     ];
 
@@ -84,27 +86,30 @@ class SingleProduct extends Composer
                     }
 
                     return $array;
-
                 }, $attachment_ids);
 
                 return [
                     'product'         => $product,
                     'product_id'      => $product->get_id(),
-                    'permalink'       => get_permalink( $product->get_id() ),
+                    'permalink'       => get_permalink($product->get_id()),
                     'title'           => $product->get_title(),
                     'product_gallery' => $product_gallery,
                 ];
-
             }, $rand_posts);
         } else {
             $rand_products_galleries = false;
         }
 
-        return [
-            'artista'       => $artist[0],
-            'link'          => get_term_link($artist[0]->term_id),
+        $output = [
             'rand_products' => $rand_products_galleries,
-        ] ;
+        ];
+
+        if ($artist) {
+            $output['artista'] = $artist[0];
+            $output['link'] = get_term_link($artist[0]->term_id);
+        }
+
+        return $output;
     }
 
     public function galeria()
@@ -112,13 +117,13 @@ class SingleProduct extends Composer
         global $product;
         $attachment_ids = $product->get_gallery_image_ids();
 
-        $output = array_map( function( $att_id ) {
+        $output = array_map(function ($att_id) {
 
-            $meta = get_post_meta( $att_id );
+            $meta = get_post_meta($att_id);
 
             $array = [
-                'att_url'    => wp_get_attachment_url( $att_id ),
-                'att_srcset' => wp_get_attachment_image_srcset( $att_id ),
+                'att_url'    => wp_get_attachment_url($att_id),
+                'att_srcset' => wp_get_attachment_image_srcset($att_id),
                 'has_alt'    => false,
                 'meta'       => $meta,
             ];
@@ -128,7 +133,6 @@ class SingleProduct extends Composer
                 $array['alt'] = $meta['_wp_attachment_image_alt'];
             }
             return $array;
-
         }, $attachment_ids);
 
         return $output;
@@ -148,7 +152,7 @@ class SingleProduct extends Composer
             foreach ($product_variations_list as $idx => $variation) {
                 $variation_id = $variation["variation_id"];
                 $variation_obj = wc_get_product($variation_id);
-                
+
                 // Obtén los atributos de esta variación específica
                 $variation_attributes = $variation_obj->get_variation_attributes();
                 // Concatena los atributos y sus valores para mostrar
@@ -159,7 +163,7 @@ class SingleProduct extends Composer
 
                     // Comprueba si el atributo actual es el "product type" para omitirlo
                     if ($attribute_taxonomy !== 'pa_product-type' && $attribute_taxonomy !== 'product_type') { // Ajusta la clave de atributo según tu necesidad
-                        if($attr_value){ // Asegúrate de que el valor del atributo no esté vacío
+                        if ($attr_value) { // Asegúrate de que el valor del atributo no esté vacío
                             $attribute_value = get_term_by('slug', $attr_value, $attribute_taxonomy)->name;
                             $attributes_string[] = $attribute_value;
                         }
@@ -167,7 +171,7 @@ class SingleProduct extends Composer
                 }
                 $attributes_string = implode(', ', $attributes_string);
 
-            
+
                 $output[$idx] = [
                     'product_type'    => $product_type,
                     'variation_id'    => $variation_id,
@@ -181,10 +185,9 @@ class SingleProduct extends Composer
             }
 
             return $output;
-
         } else {
             $attributes = $product->get_attributes();
-        
+
             // Recoger y concatenar todos los atributos excluyendo 'product_type'
             $attributes_string = [];
             foreach ($attributes as $attribute_name => $attribute) {
@@ -193,18 +196,16 @@ class SingleProduct extends Composer
                 }
             }
             $attributes_string = implode(', ', $attributes_string);
-        
+
             if ($product_type == 'simple') {
                 $output = [];
                 $output[0] = [
                     'product_type' => $product_type,
                     'attributes'   => $attributes_string,
                 ];
-        
+
                 return $output;
             }
         }
-
     }
-
 }
