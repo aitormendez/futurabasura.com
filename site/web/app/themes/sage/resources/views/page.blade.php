0ace8fadba1092002b81b947a048cec20aca3434
Aitor Méndez <aitor@e451.net>
2024-09-27 17:52:00 +0200

    Restaurar sage: html-forms funciona

--- site/web/app/themes/sage/resources/views/page.blade.php
+++ site/web/app/themes/sage/resources/views/page.blade.php
@@ -3,6 +3,7 @@
     @while (have_posts())
         @php(the_post())
         @include('partials.page-header')
+        {!! do_shortcode('[hf_form slug="participate"]') !!}
         @includeFirst(['partials.content-page', 'partials.content'])
     @endwhile
 @endsection
