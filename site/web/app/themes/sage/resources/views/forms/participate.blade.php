c6109b5e91c22fbec906073554f18356ddad5aec
Aitor Méndez <aitor@e451.net>
2024-09-30 07:38:49 +0200

    Test formulario

--- site/web/app/themes/sage/resources/views/forms/participate.blade.php
+++ site/web/app/themes/sage/resources/views/forms/participate.blade.php
@@ -3,7 +3,7 @@
     <input name="surname" type="text" placeholder="Surame" required>
     <input name="emailAddress" type="email" placeholder="Email Address" required>
     <textarea id="message" name="message" placeholder="message"></textarea>
-    <label for="archivo">Upload ZIP file with documentation</label>
+    {{-- <label for="archivo">Upload ZIP file with documentation</label> --}}
     {{-- <input type="file" id="file" name="file" accept=".zip" required> --}}
     <input type="submit" value="Submit" />
 </x-html-forms>
