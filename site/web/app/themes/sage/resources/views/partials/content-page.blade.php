60d877991f5d337c8660831e8f85354efdc566f3
Aitor Méndez <aitor@e451.net>
2024-10-12 11:38:36 +0200

    Eliminar padding x en páginas

--- site/web/app/themes/sage/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-page.blade.php
@@ -1,10 +1,10 @@
-<div class="contenido prose bg-white prose-fb py-6 md:py-20 text-xl px-6 md:px-none">
-  @php(the_content())
+<div class="contenido md:px-none prose prose-fb bg-white py-6 text-xl md:py-20">
+    @php(the_content())
 </div>
 
 
 @if ($pagination)
-  <nav class="page-nav" aria-label="Page">
-    {!! $pagination !!}
-  </nav>
+    <nav class="page-nav" aria-label="Page">
+        {!! $pagination !!}
+    </nav>
 @endif
