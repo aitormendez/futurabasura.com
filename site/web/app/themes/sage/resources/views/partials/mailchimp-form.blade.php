da058069ae8ccbc81ac24e3c3e6e7d38f24551de
Aitor Méndez <aitor@e451.net>
2025-04-21 12:34:53 +0200

    Footer

---
+++ site/web/app/themes/sage/resources/views/partials/mailchimp-form.blade.php
@@ -0,0 +1,52 @@
+<div class="formulario flex w-full flex-col items-center">
+    <div
+        class="sombra flex w-[25vw] shrink-0 grow-0 flex-col items-center justify-center py-6 text-center text-2xl leading-tight md:w-full md:leading-relaxed">
+        Join newsletter
+    </div>
+
+    <div class="flex w-full border-y border-azul bg-azul-claro md:border-y-2">
+        <div id="mc_embed_shell" class="w-full">
+            <div id="mc_embed_signup" class="h-full">
+                <form class="h-full w-full"
+                    action="https://futurabasura.us6.list-manage.com/subscribe/post?u=291a8b51f4f3fd74d185a9ea8&amp;id=82d19ee1ff&amp;f_id=00251ce3f0"
+                    method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+                    target="_blank">
+                    <div id="mc_embed_signup_scroll" class="flex h-full">
+                        <div class="izquierda w-[50vw] shrink-0">
+                            <div class="mc-field-group h-full text-azul"><label class="hidden" for="mce-EMAIL">Email
+                                    Address<span class="asterisk">*</span></label><input type="email" name="EMAIL"
+                                    class="required email w-full bg-azul-claro px-6 py-2 font-bugrino text-2xl font-light"
+                                    id="mce-EMAIL" required="" value="" placeholder="Email">
+                            </div>
+
+                            <div id="mce-responses" class="clear foot">
+                                <div class="response" id="mce-error-response" style="display: none;"></div>
+                                <div class="response" id="mce-success-response" style="display: none;"></div>
+                            </div>
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                /* real people should not fill this in and expect good things - do not remove this
+                                or
+                                risk form bot signups */
+                                <input type="text" name="b_291a8b51f4f3fd74d185a9ea8_82d19ee1ff" tabindex="-1"
+                                    value="">
+                            </div>
+                        </div>
+
+                        <div class="clear foot shrink-1 flex w-full grow-0 justify-center py-1 pr-6">
+                            <div
+                                class="h-full w-full overflow-hidden rounded-elipse border border-azul md:w-56 md:border-2">
+                                <input type="submit" name="subscribe" id="mc-embedded-subscribe"
+                                    class="button h-full w-full cursor-pointer !bg-white text-azul transition-colors hover:!bg-azul hover:!text-white"
+                                    value="Subscribe">
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+                <div class="email-error-container"></div>
+            </div>
+            <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+        </div>
+
+    </div>
+</div>
