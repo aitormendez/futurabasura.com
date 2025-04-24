63bf955d9efd8cbb3375c566f564c25aac1dd227
Aitor Méndez <aitor@e451.net>
2025-04-24 18:43:28 +0200

    Tipografía y layout (prose)

--- site/web/app/themes/sage/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-page.blade.php
@@ -1,3 +1,15 @@
-<main class="main is-content-width" id="main">
-    @php the_content() @endphp
-</main>
+@if (is_front_page())
+    <main id="main" class="main is-content-width md:px-none pb-6 pt-0 text-xl md:pb-20">
+        @php the_content() @endphp
+    </main id="main">
+@else
+    <main id="main" class="main is-content-width md:px-none prose w-full bg-white py-6 text-xl md:py-20">
+        @php the_content() @endphp
+    </main id="main">
+@endif
+
+@if ($pagination)
+    <nav class="page-nav" aria-label="Page">
+        {!! $pagination !!}
+    </nav>
+@endif
