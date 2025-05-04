dfe86f9dc18a4961e4b2acb0ed450a848feb4994
Aitor Méndez <aitor@e451.net>
2025-05-04 13:59:02 +0200

    Checkout

--- site/web/app/themes/sage/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-page.blade.php
@@ -2,7 +2,7 @@
     <main id="main" class="main is-content-width md:px-none pb-6 pt-0 text-xl md:pb-20">
         @php the_content() @endphp
     </main>
-@elseif (is_page('cart'))
+@elseif (is_page('cart') || is_page('checkout'))
     <main id="main" class="main is-content-width md:px-none w-full py-6 text-xl md:py-20">
         @php the_content() @endphp
     </main>
@@ -11,7 +11,6 @@
         @php the_content() @endphp
     </main>
 @endif
-
 
 @if ($pagination)
     <nav class="page-nav" aria-label="Page">
